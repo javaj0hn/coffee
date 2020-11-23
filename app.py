bea9871ac8ca34963b7f31bebc492efd9c3af999
javaj0hn <johnlongbeachny@gmail.com>
2020-11-22 23:40:44 -0500

    Removed flask, using fastapi

--- app.py
+++ app.py
@@ -1,21 +1,20 @@
-from flask import Flask, request, render_template, jsonify
+from fastapi import FastAPI
+
 from urllib.request import urlopen
 import urllib.error, json, math, re
 from requests_html import HTML
-from socket import timeout
+#from socket import timeout
 import string, random, json
-import sqlite3
-import requests
-import datetime
+#import sqlite3
+#import requests
+#import datetime
 from utils.datetime_z import parse_datetime
-import pytz
-import os
-import ts3
+#import pytz
 
 # import configuration
 from conf import config
 
-app = Flask(__name__)
+app = FastAPI()
 
 # random id generator
 def random_generator(size=6, chars=string.ascii_uppercase + string.digits):
@@ -27,12 +26,12 @@ def convert(lst):
     return res_dct
 
 # landing page
-@app.route('/')
+@app.get('/')
 def index():
     return "Hi"
 
 # osrs population
-@app.route('/osrs/population')
+@app.get('/osrs/population')
 def osrsPopulation():
     # make request to world select page
     try:
@@ -71,8 +70,8 @@ def osrsPopulation():
     return json.dumps(data)
 
 # osrs stat lookup
-@app.route('/osrs/stats/<string:rsn>')
-def osrsLookup(rsn):
+@app.get('/osrs/stats/{rsn}')
+def osrsLookup(rsn: str):
     # check if rsn is empty
     if not rsn:
       return json.dumps(False)
@@ -160,130 +159,3 @@ def osrsLookup(rsn):
     stats['overall_xp'] = results[0]
 
     return json.dumps(stats)
-
-# osrs xp tracker
-@app.route('/osrs/xptracker')
-def xptracker():
-  return render_template("xptracker.html")
-
-@app.route('/osrs/xptracker/start')
-def start_xptracker():
-  tracker = []
-  data = {}
-  # takes in list of rsns, clan name, fight setting etc
-  data['event_id'] = random_generator()
-  data['status'] = "start"
-  data['clan_name'] = request.args.get('clan_name')
-  data['event_name'] = request.args.get('event_name')
-  data['server'] = request.args.get('server')
-  data['track_noncombat'] = request.args.get('noncombat')
-  rsns = request.args.get('rsns')
-  data['rsns'] = list(rsns.split("\n"))
-  tracker.append(data.copy())
-
-  # look rsns up
-  for rsn in data['rsns']:
-        stats = json.loads(osrsLookup(rsn))
-        if (stats == False):
-              # add to invalid list -- try again later?
-              pass
-        else:
-            user = {}
-            user['rsn'] = stats['rsn']
-            user['attack_xp'] = stats['attack_xp']
-            user['strength_xp'] = stats['strength_xp']
-            user['defence_xp'] = stats['defence_xp']
-            user['hitpoints_xp'] = stats['hitpoints_xp']
-            user['ranged_xp'] = stats['ranged_xp']
-            user['magic_xp'] = stats['magic_xp']
-
-        # if we want to track non combat stats
-        if (data['track_noncombat']):
-              pass
-
-        # add user to list
-        tracker.append(user.copy())
-  
-  # write to json file
-  with open("data/xptracker/s_" + data['event_id'] + ".json", 'w') as fp:
-    json.dump(tracker, fp)
-
-  return jsonify(True, tracker)
-
-@app.route('/osrs/xptracker/end')
-def end_xptracker():
-  tracker = []
-
-  # get event_id
-  event_id = request.args.get('event_id')
-
-  # TODO: check if file exists
-
-  # read start json file
-  with open("data/xptracker/s_" + event_id + ".json", 'r') as f:
-    data = json.load(f)
-
-  # event details
-  details = {}
-  details['event_id'] = data[0]['event_id']
-  details['clan_name'] = data[0]['clan_name']
-  details['event_name'] = data[0]['event_name']
-  details['date'] = "N/A"
-  details['mvp'] = "N/A"
-  details['top_melee'] = "N/A"
-  details['top_ranged'] = "N/A"
-  details['top_ranged'] = "N/A"
-  tracker.append(details.copy())
-
-  i = 1
-  while i < len(data): 
-    stats = json.loads(osrsLookup(data[i]['rsn']))
-    if (stats == False):
-          pass
-
-    user = {}
-    user['rsn'] = data[i]['rsn']
-    user['attack_xp'] = int(stats['attack_xp']) - (int(data[i]['attack_xp']))
-    user['strength_xp'] = int(stats['strength_xp']) - (int(data[i]['strength_xp']))
-    user['defence_xp'] = int(stats['defence_xp']) - (int(data[i]['defence_xp']))
-    user['hitpoints_xp'] =int(stats['hitpoints_xp']) - (int(data[i]['hitpoints_xp']))
-    user['ranged_xp'] = int(stats['ranged_xp']) - (int(data[i]['ranged_xp']))
-    user['magic_xp'] = int(stats['magic_xp']) - (int(data[i]['magic_xp']))
-    user['overall_xp'] = (user['attack_xp'] + user['strength_xp'] 
-    + user['defence_xp'] + user['hitpoints_xp'] + user['ranged_xp'] + user['magic_xp'])
-
-    # add user to list
-    tracker.append(user.copy())
-
-    # inc
-    i += 1
-
-  # calc avgs
-  # write results to json file
-  with open("data/xptracker/r_" + data[0]['event_id'] + ".json", 'w') as fp:
-    json.dump(tracker, fp)
-
-  # return results
-  return jsonify(True, tracker)
-
-def get_overall(person):
-    return person.get('overall_xp')
-
-@app.route('/osrs/xptracker/results/<string:id>')
-def publicResults(id):
-  # check for id etc
-  # open json file
-  with open("data/xptracker/r_" + id +".json", 'r') as fp:
-    data = json.load(fp)
-
-  header = data[0]
-  data.pop(0)
-  data.sort(key=get_overall, reverse=True)
-  return render_template('results.html', header=header, data=data)
-
-if __name__ == '__main__':
-  app.run(
-    port=config["PORT"],
-    host=config["HOST"],
-    debug=config["DEBUG"]
-  )
